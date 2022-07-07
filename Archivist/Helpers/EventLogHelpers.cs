d0ef9d1a5fd8ffeafaddef8b45bdfd4b926fc434
Chris Turnbull <dev@silentdiver.net>
2022-07-07 13:34:03 +0800

    Capotured EventLog exception

--- Archivist/Helpers/EventLogHelpers.cs
+++ Archivist/Helpers/EventLogHelpers.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using static Archivist.Enumerations;
 
 namespace Archivist.Helpers
@@ -10,16 +11,24 @@ namespace Archivist.Helpers
     {
         internal static void WriteEntry(string text, enSeverity severity)
         {
-#pragma warning disable CA1416 // Validate platform compatibility
-            var type = severity switch
+            try
             {
-                enSeverity.Error => EventLogEntryType.Error,
-                enSeverity.Warning => EventLogEntryType.Warning,
-                _ => EventLogEntryType.Information
-            };
+    #pragma warning disable CA1416 // Validate platform compatibility
+                var type = severity switch
+                {
+                    enSeverity.Error => EventLogEntryType.Error,
+                    enSeverity.Warning => EventLogEntryType.Warning,
+                    _ => EventLogEntryType.Information
+                };
 
-            EventLog.WriteEntry("Archivist", message: text, type: type);
-#pragma warning restore CA1416 // Validate platform compatibility
+                EventLog.WriteEntry("Archivist", message: text, type: type);
+    #pragma warning restore CA1416 // Validate platform compatibility
+            }
+            catch (Exception ex)
+            {
+                // Failed to write to event log, don't throw 
+                Console.WriteLine($"Cannot write to event log, exception {ex.Message}");
+            }
         }
     }
 }
